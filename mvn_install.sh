2f54ddc203da6d3ac8c4a87609ea288f2884dd47
aakumar07 <devopss2aws@gmail.com>
2023-11-14 05:31:21 +0000

    Smaple project as implemented ad pushing to remoto repo

---
+++ mvn_install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Java Installation
+yum install -y java-1.8.0-openjdk-devel.x86_64
+
+# Maven installation
+wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+yum install -y apache-maven
+
+
+
