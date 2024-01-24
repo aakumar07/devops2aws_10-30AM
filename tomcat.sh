99333accfc9dd6aa9942bfb94a547c963d98ac32
aakumar07 <126548776+aakumar07@users.noreply.github.com>
2024-01-24 12:15:54 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -4,10 +4,8 @@
 yum install java-1.8.0-openjdk.x86_64 -y
 
 # Installing Tomcat
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.82/bin/apache-tomcat-9.0.82.tar.gz
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz
+
 
 # Untar the file
 tar -xvf apache-tomcat-9.0.82.tar.gz
-
-# Change the directory
-cd /home/ec2-user/apache-tomcat-9.0.82
